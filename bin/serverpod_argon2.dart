a5193ff2c4eb5c352788a432cdfc40ad209be0bb
Kasper Overgård Nielsen <kasper@byolimit.com>
2026-09-18 12:34:35 +0200

    chore: Replace console template with package scaffold

--- bin/serverpod_argon2.dart
+++
@@ -1,5 +0,0 @@
-import 'package:serverpod_argon2/serverpod_argon2.dart' as serverpod_argon2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${serverpod_argon2.calculate()}!');
-}
--- test/serverpod_argon2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:serverpod_argon2/serverpod_argon2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
