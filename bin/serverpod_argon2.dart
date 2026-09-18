f07b5a4fabd2dd2497755f8d6f15c578e11cacbe
Kasper Overgård Nielsen <kasper@byolimit.com>
2026-09-18 12:31:05 +0200

    init

---
+++ bin/serverpod_argon2.dart
@@ -0,0 +1,5 @@
+import 'package:serverpod_argon2/serverpod_argon2.dart' as serverpod_argon2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${serverpod_argon2.calculate()}!');
+}
---
+++ test/serverpod_argon2_test.dart
@@ -0,0 +1,8 @@
+import 'package:serverpod_argon2/serverpod_argon2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
